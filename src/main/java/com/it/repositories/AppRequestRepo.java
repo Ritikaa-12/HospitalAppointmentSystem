f25f67409a673746e0542f19c516710d17cea003
Ritikaa-12 <166943766+Ritikaa-12@users.noreply.github.com>
2026-04-13 12:10:30 +0530

    Update AppRequestRepo.java

--- src/main/java/com/it/repositories/AppRequestRepo.java
+++ src/main/java/com/it/repositories/AppRequestRepo.java
@@ -2,12 +2,15 @@ package com.it.repositories;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.it.entities.AppointmentRequest;
+import com.it.entities.Doctor;
 import com.it.entities.Patient;
 
 @Repository
@@ -18,4 +21,11 @@ List<AppointmentRequest> findRequests(LocalDate appointmentDate);
 
 @Query("SELECT R from AppointmentRequest as R where appointmentDate>=?1 and patient=?2 ORDER BY appointmentDate")
 List<AppointmentRequest> findPatientRequests(LocalDate appointmentDate,Patient patient);
+
+Optional<AppointmentRequest> findByPatientAndDoctorAndAppointmentDateAndActiveStatus(
+        Patient patient,
+        Doctor doctor,
+        LocalDate appointmentDate,
+        Boolean activeStatus
+);
 }
